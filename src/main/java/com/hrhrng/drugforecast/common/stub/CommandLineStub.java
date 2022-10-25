13191f2925c415912bb8bac4c6615e4f1faed51b
hrhrng <hrhrng@foxmail.com>
2022-10-25 22:12:47 +0800

    命令行工具

--- src/main/java/com/hrhrng/drugforecast/common/stub/CommandLineStub.java
+++ src/main/java/com/hrhrng/drugforecast/common/stub/CommandLineStub.java
@@ -2,6 +2,13 @@ package com.hrhrng.drugforecast.common.stub;
 
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CommandLineStub {
 
@@ -9,8 +16,31 @@ public class CommandLineStub {
     /**
      * 这个路径下存放脚本
      */
-    String baseUrl = "~/script/";
+    static String baseUrl = "D:/forest/util/";
+
+    static Runtime runtime = Runtime.getRuntime();
 
 
+    public static List<String> exec(String command) throws IOException, InterruptedException {
+        Process process = runtime.exec("ipconfig");
+        InputStream input = process.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        String szline;
+        List<String> ret = new ArrayList<>();
+        while ((szline = reader.readLine()) != null) {
+            ret.add(szline);
+        }
+        reader.close();
+        process.waitFor();
+        process.destroy();
+        return ret;
+    }
 
+    public static List<String> execWithUtil(String command) throws IOException, InterruptedException {
+        return exec(baseUrl + command);
+    }
 }
+
+
+
+
